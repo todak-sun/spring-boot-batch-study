e9b05997645a9ddc3306bf0c2910c14dfe434b1c
YongJoo Sun <yjsun@tradlinx.com>
2021-04-29 19:42:12 +0900

    initial commit

---
+++ src/main/java/io/todak/study/springbootbatchstudy/SpringBootBatchStudyApplication.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootbatchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBatchStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/todak/study/springbootbatchstudy/SpringBootBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootbatchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
